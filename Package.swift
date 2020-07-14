a624f964932c63d6b67ef2debeefe360cd03d9db
Serhii <zh.send@gmail.com>
2020-07-14 17:32:23 +0700

    Initial commit Structure created from scratch

--- Package.swift
+++ Package.swift
@@ -5,11 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "PhoneNumberKit",
+	platforms: [
+		.iOS(.v13)
+	],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PhoneNumberKit",
-            targets: ["PhoneNumberKit"]),
+            targets: ["PhoneNumberKit"]
+		),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +24,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PhoneNumberKit",
-            dependencies: []),
+            dependencies: [],
+			resources: [
+				.process("Resources/PhoneNumberMetadata.json")
+			]
+		),
         .testTarget(
             name: "PhoneNumberKitTests",
-            dependencies: ["PhoneNumberKit"]),
+            dependencies: ["PhoneNumberKit"]
+		),
     ]
 )
